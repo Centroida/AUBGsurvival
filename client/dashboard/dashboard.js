1981db27af66d026d3fa161719a1cb489f386230
StefanL19 <stlazov@abv.bg>
2016-03-03 23:50:55 +0200

    assigning between users

--- client/dashboard/dashboard.js
+++ client/dashboard/dashboard.js
@@ -34,6 +34,16 @@ Template.userDashboard.helpers({
 Template.userDashboard.events({
     "click .js-kill-target":function(event){
         var inputId = $(".targetId").val();
-        console.log(inputId);
+        var userId = Meteor.userId();
+        var user = Meteor.users.findOne({_id:userId});
+        var actualVictimId = user.profile.assigned;
+        var actualVictim = Meteor.users.findOne({_id:actualVictimId});
+        if (inputId == actualVictim.profile.token) {
+            Meteor.call("destroyUser", actualVictimId);
+            Meteor.call("changeAssigned", actualVictimId, userId);
+            console.log("You killed the user. How you dare?");
+        } else {
+            console.log("This is not the right id")
+        }
     }
 });
--- shared/methods.js
+++ shared/methods.js
@@ -6,5 +6,16 @@ Meteor.methods({
        if (user) {
            Meteor.users.update({_id:victimId}, {$set : {"profile.statusAssigned":true, "profile.assignedTo":arrayAssignedTo}});
        }
+   },
+   
+   destroyUser: function(victimId){
+       Meteor.users.update({_id:victimId}, {$set: {"profile.status":"killed"}});
+   },
+   
+   //Here happens the actual assignment of the target of the victim to the killer
+   changeAssigned: function(victimId, killerId){
+       var victim = Meteor.users.findOne({_id:victimId});
+       var assignedVictim = victim.profile.assigned;
+       Meteor.users.update({_id:killerId}, {$set: {"profile.assigned":assignedVictim}});   
    } 
 });
