0adbee16562d0a4faffafb2b6ed5922cbdbc1e3c
Hristo Georgiev <hgeorgievkzr@gmail.com>
2016-04-10 21:38:23 +0300

    changing fonts

--- server/startup.js
+++ server/startup.js
@@ -1,9 +1,9 @@
 Meteor.startup(function () {
-
+	process.env.MAIL_URL = 'smtp://sandboxc4edb1042fcb41e6bd41e5b1ba6fdcea:8b73b3b9c423049867174dd585b2dda2.mailgun.org:587'
   //seed data
 	if(Meteor.users.find({}).count() == 0) {
 
-    process.env.MAIL_URL = 'smtp://sandboxc4edb1042fcb41e6bd41e5b1ba6fdcea:8b73b3b9c423049867174dd585b2dda2.mailgun.org:587'
+
     var adminPassEnv = process.env.ADMIN_PASSWORD;
 
 
