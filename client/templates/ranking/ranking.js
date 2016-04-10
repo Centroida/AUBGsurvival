4e0e9261413d458b88d61bd931e16d08187edfcf
Hristo Georgiev <hgeorgievkzr@gmail.com>
2016-04-10 21:32:29 +0300

    sort

--- client/templates/ranking/ranking.js
+++ client/templates/ranking/ranking.js
@@ -1,6 +1,6 @@
 Template.ranking.helpers({
       rankings:function(){
-        var users = Meteor.users.find({"profile.kills":{ $gt: 0 } }, {sort: {"profile.kills": -1, "profile.lastKill": 1}})
+        var users = Meteor.users.find({"profile.kills":{ $gt: 0 } }, {sort: {"profile.kills": -1, "profile.lastKill": 1 , "profile.firstName": -1 }});
         if(users){
             return users;
         }
